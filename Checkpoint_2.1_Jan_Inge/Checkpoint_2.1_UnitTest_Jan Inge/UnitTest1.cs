1141f3cc07488672b53e2fc312b757348e1b91d6
beachman90 <jistrandman@gmail.com>
2023-11-13 14:02:23 +0100

    Removed a } and added testing for AddPlayer method.

--- Checkpoint_2.1_Jan_Inge/Checkpoint_2.1_UnitTest_Jan Inge/UnitTest1.cs
+++ Checkpoint_2.1_Jan_Inge/Checkpoint_2.1_UnitTest_Jan Inge/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Checkpoint_2._1_Jan_Inge;
+
 namespace Checkpoint_2._1_UnitTest_Jan_Inge
 {
     public class Tests
@@ -8,9 +10,19 @@ namespace Checkpoint_2._1_UnitTest_Jan_Inge
         }
 
         [Test]
-        public void Test1()
+        public void AddToList_ReturnListContent()
         {
-            
+            Team myTeam = new("Gutta");
+            myTeam.AddPlayer(new("Dude", "7"));
+            myTeam.AddPlayer(new("Miss", "11"));
+            myTeam.AddPlayer(new("Mr Buttons", "16"));
+
+            int expected = myTeam.players.Count();
+            int actual = 3;
+
+            Assert.That(actual, Is.EqualTo(expected));
+
+
         }
     }
 }
